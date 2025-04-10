e1410bb36bfe5bf055b741f8ba011c1419577554
Dorsamnv <dorsamnv@gmail.com>
2025-04-10 23:40:30 +0330

    @for tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,15 @@ import { Course } from './model/course';
 })
 export class AppComponent {
 
- coreCourse = COURSES[0]
- rxjsCourse = COURSES[1]
- ngrxCourse = COURSES[2]
-
+ courses= [...COURSES]
+     
 
  onCourseSelected(course:Course){
     console.log(course)
+ }
+ 
+ trackCourse(index:number, course:Course){
+    return course.id
  }
 
 }
