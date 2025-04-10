45be4b444a3fd0aa7b7341cdba0f70317e1980df
Dorsamnv <dorsamnv@gmail.com>
2025-04-10 19:38:29 +0330

    build components

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input({required:true})
+  course:Course
+  
+}
